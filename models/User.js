ac8bbc137d76484b41f4da8af8083fe489d37bb4
kristiyan <kristiyan.tefov@icloud.com>
2023-05-05 21:19:41 +0100

    model user and relationships  complete

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-gi const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
 class User extends Model {}
