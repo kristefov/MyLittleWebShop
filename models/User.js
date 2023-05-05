263a796bf8633a6d2f7bdfdfe21597c39f2fcab6
kristiyan <kristiyan.tefov@icloud.com>
2023-05-05 21:13:58 +0100

    model user and relationships  complete

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id"
+      }
+  }
+},
+  {
+    sequelize,
+    freezeTableName: true,
+    modelName: "user",
+  }
+
+);
+module.export = User;
---
+++ models/index.js
@@ -0,0 +1,40 @@
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+const User = require('./User');
+
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "SET NULL"
+})
+// Products belongsTo User
+Product.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL"
+})
+// Categories have many Products
+Category.hasMany(Product)
+// User have many Products
+User.hasMany(Product)
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+})
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag, 
+  foreignKey: "tag_id",
+})
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+  User
+};
