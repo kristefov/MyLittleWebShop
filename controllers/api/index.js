c993080e5d44242f676a75342abe37f67aaf1e9a
dExIT <dexit@dyc.lv>
2023-05-09 20:17:05 +0100

    Finally please work git status!

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 
 const userRoutes = require("./userRoutes");
-router.use("/users", userRoutes);
 
-const productRoutes = require("./productRoutes");
-router.use("/products", productRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
