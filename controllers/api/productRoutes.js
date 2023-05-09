b758bae25b980972cccba705bdde2bb58c89b605
kristiyan <kristiyan.tefov@icloud.com>
2023-05-09 19:41:14 +0100

    product routes created

---
+++ controllers/api/productRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const { Product }  = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const productData = await Product.findAll(req.body);
+    res.status(200).json(blogData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
