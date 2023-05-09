75c338419370a60f8a1b652d2b78071425cb2684
kristiyan <kristiyan.tefov@icloud.com>
2023-05-09 19:42:00 +0100

    product routetes fixed

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -4,7 +4,7 @@ const { Product }  = require("../../models");
 router.get("/", async (req, res) => {
   try {
     const productData = await Product.findAll(req.body);
-    res.status(200).json(blogData);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
