3e5a352f38eafd762a413f98959f697901b825de
dExIT <dexit@dyc.lv>
2023-05-10 00:12:28 +0100

    Added tons of things

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -33,13 +33,25 @@ router.get('/', withAuth, async (req, res) => {
         }
         res.render('login');
     });
+    router.get('/logout', async (req, res) => {
+        if (!req.session.logged_in) {
+            res.redirect("/login");
+            return;
+        }else {
+           
+            req.session.destroy(() => {
+                res.redirect("/login");
+            });
+        }
+        //res.render('login');
+    });
 
     router.get('/signup', async (req, res) => {
         if (req.session.logged_in) {
             res.redirect("/");
             return;
         }
-        res.render('register');
+        res.render('signUp');
     })
 
     router.get('/home', async (req, res) => {
@@ -47,9 +59,32 @@ router.get('/', withAuth, async (req, res) => {
             res.redirect("/");
             return;
         }
-        res.render('homepage');
+        res.render('homepage', {
+            products,
+            logged_in: req.session.logged_in,
+        });
     })
 
+    router.get('/checkout', async (req, res) => {
+        if (req.session.logged_in) {
+            res.redirect("/");
+            return;
+        }
+        res.render('checkout', {
+            products,
+            logged_in: req.session.logged_in,
+        } );
+    })
+    router.get('/cart', async (req, res) => {
+        if (req.session.logged_in) {
+            res.redirect("/");
+            return;
+        }
+        res.render('cart', {
+            products,
+            logged_in: req.session.logged_in,
+        } );
+    })
 
     
     module.exports = router;
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,6 @@
 const logout = async () => {
   // Make a POST request to destroy the session on the back end
-  const response = await fetch('/api/logout', {
+  const response = await fetch('/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
