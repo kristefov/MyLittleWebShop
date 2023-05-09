5a8f51c092da0ed37d67c6752dd4c17a10d94eb1
kristiyan <kristiyan.tefov@icloud.com>
2023-05-09 19:24:00 +0100

    user route fixed

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,4 @@
-
 DROP DATABASE IF EXISTS shopping_cart_db;
-
 CREATE DATABASE shopping_cart_db;
 
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 
 const userRoutes = require("./userRoutes");
 
-router.use("/", userRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
