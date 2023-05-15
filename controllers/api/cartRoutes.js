a6dae91de40f393af8889a11fb1ac942b1f5b118
kristiyan <kristiyan.tefov@icloud.com>
2023-05-15 20:21:01 +0100

    cart routes complete

---
+++ controllers/api/cartRoutes.js
@@ -0,0 +1,89 @@
+const router = require("express").Router();
+const { User, Product, Cart } = require("../../models");
+
+// The `/api/categories` endpoint
+
+router.get("/", async (req, res) => {
+  if (req.session.logged_in) {
+    try {
+      const cartData = await Cart.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ["id"],
+          },
+          {
+            model: Product,
+            attributes: ["id", "product_name", "price", "stock", "category_id"],
+          },
+        ],
+      });
+      res.status(200).json(cartData);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
+router.post("/", async (req, res) => {
+  
+  if (req.session.logged_in) {
+    try {
+      const cartData = await Cart.create({
+        ...req.body,
+        user_id: req.session.user_id,
+        product_id: req.body.product.id,
+      });
+      if (!cartData) {
+        res.status(404).send("No Product with that ID");
+      } else {
+        res.status(200).json(cartData);
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
+router.put("/:id", async (req, res) => {
+    if (req.session.logged_in) {
+  try {
+    const cartData = await Cart.update(
+      {
+        id: req.body.category.id,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!cartData) {
+      res.status(400).send("No product with that ID");
+    }
+    res.status(200).json(cartData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}});
+
+router.delete("/:id", async (req, res) => {
+    if (req.session.logged_in) {
+  try {
+    const cartData = await cart.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!cartData) {
+      res.status(400).send("Cart Empty");
+    }
+    res.status(200).json(cartData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,13 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 const userRoutes = require("./userRoutes");
+const cartRoutes = require("./cartRoutes");
 
 
 router.use("/users", userRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
+router.use('/cart', cartRoutes);
 
 module.exports = router;
