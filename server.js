874c8a573ed86ad11750930df5b30a358ca5920e
dExIT <dexit@dyc.lv>
2023-05-19 13:44:30 +0100

    added compression and favicon via express/node

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const helpers = require("./utils/helpers");
 const routes = require("./controllers");
-
-
+const compression = require('compression');
+const favicon = require('serve-favicon')
+// the two below are for future handling of added domain name
+//const vhost = require('vhost');
+//const http = require('http')
 // const helpers = require("./util/helpers");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,6 +25,12 @@ const sess = {
     db: sequelize,
   }),
 };
+// compress responses
+app.use(compression())
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+// app.use(vhost('www.example.com', app))
+// app.use(vhost('example.com', app))
+ 
 app.use(session(sess));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -33,5 +42,5 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log("Now listening to http://127.0.0.1:" + PORT));
 });
