1c9e989a0ae1eb3a0d57300a23bf5ba1f4165067
dExIT <dexit@dyc.lv>
2023-05-23 19:36:32 +0100

    fixing stripe logic in accessing correct cart data

--- controllers/api/checkoutRoutes.js
+++ controllers/api/checkoutRoutes.js
@@ -1,7 +1,7 @@
 
 const router = require("express").Router();
 const sequelizeOP = require("sequelize").Op;
-const { User, Cart, Product } = require("../../models");
+const { User, Cart, Product, CartProduct } = require("../../models");
 const stripe = require('stripe')('sk_test_51MtBULDRshRxSyZ6HHmEBs00ellZB7Vd7Bj4e1sl9MQGlvjb4HBwzwwHis55ZqJ1iFgYDns0TUYyhMadBmzcP7Zs00a1tcM42w');
 /*
 Fill out the payment details with the test card information:
@@ -18,35 +18,50 @@ router.post('/create-checkout-session', async (req, res) => {
               model: Product,
               attributes: ["id", "product_name", "price", "stock"],
             },
+            {
+              model: User,
+              attributes: ["id", "first_name", "last_name", "email"],
+            },
+
           ],
+          where: {
+            user_id: req.session.user_id,
+          }
         });
         const cartItems = cartData.map((product) => product.get({ plain: true }));
     
-console.log(cartItems);
-console.log(cartData);
+//console.log(cartItems);
+//console.log(cartData);
 
 // loop over the cartItems, and add them to a new object that is line_Items with quantity and price and name.
 const theDataset = function(data){
   const line_items = [];
-  for(let i = 0; i < data['products'].length; i++){
+  for(let i = 0; i < data.length; i++){
+    console.log(data[i].products); 
+    let zaza = data[i].products;
+   
+    for(let j = 0; j < zaza.length; j++){
     line_items.push({
       price_data: {
         currency: "gbp",
         product_data: {
-          name: data['products'][i].product_name,
+          name: zaza[j]['product_name'],
         },
-        unit_amount: (data['products'][i].price * 100).toFixed(0),
+        unit_amount: (zaza[j].price * 100).toFixed(0),
       },
       quantity: '1',
     });
+  }
 };
+console.log(line_items);
+//console.log(line_items);
 return line_items;
 };
 
 const userData = await User.findByPk(req.session.user_id);
     const stripeSession = await stripe.checkout.sessions.create({
      customer_email: userData.email,
-      line_items: theDataset(cartItems[0]),
+      line_items: theDataset(cartItems),
       mode: 'payment',
       success_url: 'https://mylittlewebshop.herokuapp.com/checkout/success',
       cancel_url: 'https://mylittlewebshop.herokuapp.com/checkout/cancel',
